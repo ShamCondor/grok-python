6f984d74c49ac7153d9e670a9f3c157f6c2f29ad
Matrix <codecolor@outlook.com>
2017-05-24 22:46:45 +0800

    单例模式

--- other/singleton.py
+++ other/singleton.py
@@ -17,15 +17,17 @@ class Singleton(type):
 
     # __call__ 是对于类实例有效，比如说Spam类，是type类的实例
     def __call__(cls, *args, **kwargs):
+        print('__call__ running')
         if cls.__instance is None:
-            # TODO 为什么是__call__ 方法，而不是__init__ 或者  __new__ ?
-            # TODO type 中，__call__ 和 __init__ 还有 __new__ 是什么关系？
             '''
             目前能理解的是，元类定义__call__方法，可以抢在类运行 __new__ 和 __init__ 之前执行，
             也就是创建单例模式的前提，在类实例化前拦截掉。
             不能理解的是，为什么是调用 super().__call__(*args, **kwargs)，而不是__new__ 或 __init__ ？
             '''
+            #  TODO type 的 __call__ 到底执行了什么？
             cls.__instance = super().__call__(*args, **kwargs)
+            # cls.__instance = super().__new__(cls, cls.__name__, *args, **kwargs)
+            # cls.__instance.__init__(cls.__instance, *args, **kwargs)
             return cls.__instance
         else:
             return cls.__instance
