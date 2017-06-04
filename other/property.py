3979c3d3b62024750f2f18702c05aa7018e92999
Matrix <codecolor@outlook.com>
2017-06-04 15:46:07 +0800

    完善关于property的示例

--- other/property.py
+++ other/property.py
@@ -9,7 +9,7 @@
 __author__ = 'blackmatrix'
 
 
-class Person(object):
+class Person:
 
     def __init__(self, age):
         self._age = age
@@ -18,11 +18,15 @@ class Person(object):
     # @property 装饰器等同于 age = property(fget=age)
     @property
     def age(self):
+        print('person property age getter')
         return self._age
+
+    # 打印看下age被装饰后的类型
+    print(type(age))
 
     @age.setter
     def age(self, value):
-        print('person proerty age setter')
+        print('person property age setter')
         self._age = value * 2
 
     @property
@@ -37,31 +41,52 @@ class Man(Person):
 
 class Student(Person):
 
-    # 如果在子类重新定义一个property,会完全覆盖掉父类的同名的property,包括里面的方法
+    # 如果在子类重新定义一个property,会完全重写掉父类的同名的property,包括里面的方法
     # 所以没有定义setter方法,age变为只读,无法赋值
-    # 实际上,这种方式是在子类重新创建了一个名为age的property对象,覆盖掉了父类名为age的property
+    # 实际上,这种方式是在子类重新创建了一个名为age的property对象,重写掉了父类名为age的property
     # age = property(fget=..., fset=None)
     @property
     def age(self):
-        print('student proerty age getter')
+        print('student property age getter')
         return self._age
 
 
 class Teacher(Person):
 
-    # 只有这种方式才能正确的覆盖掉父类的getter方法
+    # 只有这种方式才能正确的重写父类的getter方法
     @Person.age.getter
     def age(self):
-        print('teacher proerty age getter')
+        print('teacher property age getter')
         return self._age
 
-    # 同样, 这样才能正确的覆盖掉父类特性的getter方法
+    # 同样, 这样才能正确的重写掉父类特性的getter方法
     # property是个类,@Person.age.setter,这个操作相当于找到Person下,这个age(property实例)的方法fset
-    # 并用我们在子类的方法将其覆盖。
+    # 并用我们在子类的方法将其重写。
     @Person.age.setter
     def age(self, value):
-        print('teacher proerty age setter')
+        print('teacher property age setter')
         self._age = value
+        
+
+'''
+上面的方法,其实有个缺陷,我们必须清楚的知道,需要重写的propery所属的父类
+这对于单继承通常的是没有问题的,但是对于多继承就会存在问题,比如继承树中存在多个同名的property
+那么到底应该继承哪个property就会产生疑惑。
+比较合理的解决办法是,如student这个类一样,彻底重写property的实例,再使用super()方法去调用父类的方法
+'''
+
+
+class Girl(Person):
+
+    @property
+    def age(self):
+        print('girl property age getter')
+        return super().age
+
+    @age.setter
+    def age(self, value):
+        print('girl property age setter')
+        super(Girl, Girl).age.__set__(self, value)
 
 
 if __name__ == '__main__':
@@ -80,19 +105,25 @@ if __name__ == '__main__':
 
     # 如果没有实例,property会返回自身
     print(Person.age)
-    # 给类赋值一个与property的同名属性时,会覆盖掉原先的特性
-    Person.age = 24
-    print(Person.age)
-    print(type(Person.age))
+    # 给类赋值一个与property的同名属性时,会重写掉原先的特性
+    # Person.age = 24
+    # print(Person.age)
+    # print(type(Person.age))
 
-    # 子类覆盖父类的property
+    # 子类重写父类的property
     lilei = Student(age=12)
     print(lilei.age)
     # 无法赋值,报错
     # lilei.age = 13
 
+    # 子类重写父类property部分方法
     jim = Teacher(27)
     jim.age = 30
     print(jim.age)
+
+    # 子类重写父类部分方法的另一种办法
+    lucy = Girl(13)
+    lucy.age = 14
+    print(lucy.age)
 
 
