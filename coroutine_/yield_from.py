b2cc7a63fc1bcb6ed0c2fb52a3e96fcf33212992
Matrix <codecolor@outlook.com>
2018-01-20 11:51:40 +0800

    yield from的示例代码

--- coroutine_/yield_from.py
+++ coroutine_/yield_from.py
@@ -29,73 +29,73 @@ print(list(foo2([1, 2, 3, 4, 5])))
 
 
 """
-yield只能向直接调用者产出值
-不能封装到独立的函数中向函数的调用者产出值
-"""
-
-
-def bar():
-    yield foo2([1, 2, 3, 4, 5])
-
-
-print(bar())
-
-
-
-
-"""
 
 """
 
 
-# def averager():
-#     """
-#     使用yield接收数值，并求平均值
-#     :return:
-#     """
-#     count = 0
-#     total = 0.0
-#     average = 0.0
-#     while True:
-#         try:
-#             value = yield
-#             count += 1
-#             total += value
-#             average = total/count
-#         except StopIteration:
-#             return average
-#
-#
-# items_list = [[19, 23, 12, 43, 12], [76, 14, 43, 64, 17, 36], [45, 51, 64, 34, 64, 13, 75]]
-#
-#
-# for items in items_list:
-#     avg = averager()
-#     next(avg)
-#     for item in items:
-#         avg.send(item)
-#     else:
-#         try:
-#             avg.throw(StopIteration)
-#         except StopIteration as ex:
-#             print(ex)
-#
-#
-# def grouper():
-#     """
-#     定义一个委托生成器
-#     :return:
-#     """
-#     yield from averager()
-#
-# group = grouper()
-# next(group)
-# for items in items_list:
-#     group.send(items)
-#     try:
-#         group.throw(StopIteration)
-#     except StopIteration as ex:
-#         print(ex)
+def averager():
+    """
+    使用yield接收数值，并求平均值
+    捕获到StopIteration时，返回计算说得的平均值
+    :return:
+    """
+    count = 0
+    total = 0.0
+    average = 0.0
+    while True:
+        try:
+            value = yield
+            count += 1
+            total += value
+            average = total/count
+        except StopIteration:
+            return average
+
+"""
+有一个新的需求，需要通过send传入一个可迭代对象，再求平均值
+因为averager可能已经在多个模块中使用，所以无法直接修改，需要重新封装一个函数
+"""
+
+
+items_list = [[19, 23, 12, 43, 12], [76, 14, 43, 64, 17, 36], [45, 51, 64, 34, 64, 13, 75]]
+
+
+def averager2():
+    """
+    根据传入的list求平均值
+    为了减少示例代码，假设传入的list内只包含数字
+    :return:
+    """
+    value_list = yield
+
+
+for items in items_list:
+    avg = averager()
+    next(avg)
+    for item in items:
+        avg.send(item)
+    else:
+        try:
+            avg.throw(StopIteration)
+        except StopIteration as ex:
+            print(ex)
+
+
+def grouper():
+    """
+    定义一个委托生成器
+    :return:
+    """
+    yield from averager()
+
+group = grouper()
+next(group)
+for items in items_list:
+    group.send(items)
+    try:
+        group.throw(StopIteration)
+    except StopIteration as ex:
+        print(ex)
 
 
 
