22d46fe3af07dc034e4e17725da56d5fd828d888
Matrix <codecolor@outlook.com>
2018-08-24 23:31:18 +0800

    完善切片的例子

--- iteration/slice.py
+++ iteration/slice.py
@@ -3,7 +3,7 @@
 # @Time: 2017/7/29 下午10:11
 # @Author: BlackMatrix
 # @Site: https://github.com/blackmatrix7
-# @File: slices
+# @File: Slice
 # @Software: PyCharm
 import unittest
 
@@ -23,7 +23,7 @@ __author__ = 'blackmatrix'
 """
 
 
-class SlicesTestCase(unittest.TestCase):
+class SliceTestCase(unittest.TestCase):
 
     def setUp(self):
         pass
@@ -31,17 +31,26 @@ class SlicesTestCase(unittest.TestCase):
     def tearDown(self):
         pass
 
-    def testSlices01(self):
+    def testSlice01(self):
+        """
+        这个例子中，从左往右，从第0个元素开始取值，直至第3个元素结束
+        即取索引为 0 ， 1， 2的三个元素
+        :return:
+        """
         foo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         # 切片取List前三个元素
         self.assertEqual(foo[0:3], [1, 2, 3])
 
-    def testSlices02(self):
+    def testSlice02(self):
+        """
+        stop_index 省略的话，默认取到有序序列的最后一个元素为止
+        :return:
+        """
         foo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         # 切片取List后三个元素
-        self.assertEqual(foo[len(foo)-3:], [7, 8, 9])
+        self.assertEqual(foo[6:], [7, 8, 9])
 
-    def testSlices03(self):
+    def testSlice03(self):
         """
         对切片进行赋值，被赋值的切片，会根据赋值的变量长度自动调整
         :return:
@@ -55,24 +64,16 @@ class SlicesTestCase(unittest.TestCase):
         bar[7:] = [20, 30, 40, 50, 60, 70]
         self.assertEqual(bar, [1, 2, 3, 4, 5, 6, 7, 20, 30, 40, 50, 60, 70])
 
-    def testSlices04(self):
+    def testSlice04(self):
         """
         利用切片让有序序列倒序
+        当start_index和stop_index都省略的时候，则取出完整的有序序列
         :return:
         """
         foo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         self.assertEqual(foo[: :-1], [9, 8, 7, 6, 5, 4, 3, 2, 1])
 
-    def testSlices05(self):
-        """
-        切片时，stop_index为负数，起始位置从右往左数
-        :return:
-        """
-        foo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-        # -3 即从右往左数3个元素，为 [7, 8, 9]
-        self.assertEqual(foo[-5:  -2], [5, 6, 7])
-
-    def testSlices06(self):
+    def testSlice05(self):
         """
         切片时，start_index为负数，起始位置从右往左数
         :return:
@@ -80,6 +81,35 @@ class SlicesTestCase(unittest.TestCase):
         foo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
         # -3 即从右往左数3个元素，为 [7, 8, 9]
         self.assertEqual(foo[-3: ], [7, 8, 9])
+
+    def testSlice06(self):
+        """
+        切片时，stop_index为负数，起始位置从右往左数
+        start_index 和 stop_index 都为负数时，也可以混用，只要确保能正确取出值即可
+        :return:
+        """
+        foo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        # -3 即从右往左数3个元素，为 [7, 8, 9]
+        self.assertEqual(foo[-5:  -2], [5, 6, 7])
+
+    def testSlice07(self):
+        """
+        list对象进行切片，本质是__getitem__获取接受一个Slice的对象
+        同理，如果自定义一种类型，实现__getitem__魔法方法，能够处理slice对象
+        也可以实现切片操作
+        :return:
+        """
+        slice_obj = slice(0, 3)
+        foo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertEqual(foo.__getitem__(slice_obj), foo[0: 3])
+
+    def testSlice08(self):
+        """
+        当步长step不为0时，则每step个元素取一个元素
+        :return:
+        """
+        foo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+        self.assertEqual(foo[::2], [1, 3, 5, 7, 9])
 
 
 if __name__ == '__main__':
