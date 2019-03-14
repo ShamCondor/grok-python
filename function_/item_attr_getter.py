d0101a9a2bfe1febce4c8ff59669e79ce25f4104
Matrix <blackmatrix7@users.noreply.github.com>
2019-03-14 13:50:45 +0800

    优化补充itemgetter, attrgetter的例子

--- function_/item_attr_getter.py
+++ function_/item_attr_getter.py
@@ -5,6 +5,7 @@
 # @Site: https://github.com/blackmatrix7
 # @File: itemgetter
 # @Software: PyCharm
+import unittest
 from operator import itemgetter, attrgetter
 
 __author__ = 'blackmatrix'
@@ -16,35 +17,63 @@ Python标准库中的operator的itemgetter, attrgetter方法
 1. itemgetter可以从目标中获取元素
 2. attrgetter可以从目标中获取属性
 3. 两者都可以用于替代简单的获取元素或属性的表达式
-4. itemgetter接收多个参数时，将参数值作为索引，提取目标多个元素，并组成tuple返回
+4. itemgetter接收多个参数时，根据参数值提取目标多个元素，并组成tuple返回
 5. attrgetter接收多个参数时，将参数值作为属性值，提取目标多个元素，并组成tuple返回
 6. attrgetter接收的参数如果带点号"."，会深入嵌套对象，提取指定属性
 """
 
-get_name = lambda user: user['name']
 
+class Person:
+
+    def __init__(self, name, city):
+        self.name = name
+        self.city = city
+        self.son = None
+
+# 示例使用，本身不符合PEP8规范，应使用def
+get_name = lambda user: user.name
 get_address = lambda user: getattr(user, 'address')
 
 
-class User:
+class TestOperatorFunc(unittest.TestCase):
+    def setUp(self):
+        pass
 
-    def __init__(self, name, address):
-        self.name = name
-        self.address = address
+    def tearDown(self):
+        pass
 
+    def testAttrGetter(self):
+        # 实例化两个Person
+        father = Person('张三', '厦门')
+        son = Person('张一一', '上海')
+        # 构建父子关系
+        father.son = son
+        # 获取父亲姓名
+        father_name = attrgetter('name')(father)  # 张三
+        # 可以替代简单的lambda表达式
+        self.assertEqual(father_name, get_name(father))
+        # 同时获取姓名和城市
+        name_city = attrgetter('name', 'city')(father)
+        # 返回包含有姓名和城市的元组
+        self.assertTrue(isinstance(name_city, tuple))
+        self.assertEqual(attrgetter('name', 'city')(father), ('张三', '厦门'))
+        # 处理含有点号的情况，自动获取son实例的name属性
+        son_name = attrgetter('son.name')(father)
+        self.assertTrue(isinstance(son_name, str))
+        self.assertEqual(son_name, '张一一')
 
-laowang = {'name': '老王', 'address': '隔壁'}
-laoli = User('老李', '楼下')
+    def testItemGetter(self):
+        # itemgetter可用于list
+        citys = ['厦门', '上海', '北京']
+        result = itemgetter(0, 1)(citys)
+        self.assertTrue(isinstance(result, tuple))
+        self.assertEqual(result, ('厦门', '上海'))
+        # 也可用于dict
+        father = {'name': '张三', 'city': '厦门'}
+        name_city = itemgetter('name', 'city')(father)
+        self.assertTrue(isinstance(result, tuple))
+        self.assertEqual(name_city, ('张三', '厦门'))
 
 
 if __name__ == '__main__':
-    # itemgetter('name') 和 get_name 作用是相同的
-    # 都是接受一个对象，获取对象key为name的值
-    # 主要用来替代只是为了获取value的lambda函数
-    print(itemgetter('name')(laowang))
-    print(get_name(laowang))
-
-    # 同样的，attrgetter('address')也是等同于get_address
-    # 用来替代只为了获取attr的lambda函数
-    print(attrgetter('address')(laoli))
-    print(get_address(laoli))
+    unittest.main()
